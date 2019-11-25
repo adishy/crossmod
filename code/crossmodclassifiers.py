c259a0ceeadbb27c8f63f02a6412fb05bf7a4a0b
Ubuntu <ubuntu@ip-172-31-24-153.us-east-2.compute.internal>
2019-11-25 17:33:28 +0000

    Updated crossmodclassifiers.py

--- code/crossmodclassifiers.py
+++ code/crossmodclassifiers.py
@@ -4,6 +4,7 @@ from multiprocessing import Pool
 import subprocess
 import re
 from crossmodconsts import *
+import time
 
 class CrossmodClassifiers:    
     SUBREDDIT_CLASSIFIERS = "subreddit"
@@ -19,14 +20,16 @@ class CrossmodClassifiers:
         clfs_type = kwargs['clfs_type']
         clfs_ids = kwargs['clfs_ids']
 
-        self.clfs_pool = Pool(len(clfs_ids))
+        self.clfs_pool = Pool(10)
         self.clfs_arguments = [{'clfs_type': clfs_type, 
                                 'clfs_id': clf, 
                                 'input_comment': input_comment} for clf in clfs_ids]
 
         # [ {'clfs_id': 'science', 'clfs_prediction': 1, 'clfs_type': SUBREDDIT }, {'clfs_id': 'space', 'clfs_prediction': 1, 'clfs_type': SUBREDDIT } ]
         self.clfs_predictions = self.clfs_pool.map(CrossmodClassifiers.run_classifier, self.clfs_arguments)
-    
+        self.clfs_pool.close()
+        self.clfs_pool.join()
+        
         self.total_clfs = len(clfs_ids)
 
         result = {
@@ -67,9 +70,14 @@ class CrossmodClassifiers:
         
     @staticmethod
     def run_classifier(arguments):
+        start_total = int(round(time.time() * 1000))
+
+        print('PID: ' + str(os.getpid()))
         input_comment = arguments['input_comment']
         clfs_type = arguments['clfs_type']
         clfs_id = arguments['clfs_id']
+
+        start = int(round(time.time() * 1000))
 
         if clfs_type == CrossmodClassifiers.SUBREDDIT_CLASSIFIERS:
             clf_path = CrossmodConsts.get_subreddit_classifier(clfs_id)
@@ -79,14 +87,30 @@ class CrossmodClassifiers:
 
         clf = fasttext.load_model(clf_path)
         input_comment = CrossmodClassifiers.process_input_comment(input_comment)
+        end = int(round(time.time() * 1000))
+
+        print("Loading: ", end - start)
+        
+        start =  int(round(time.time() * 1000))
+
         clf_prediction = clf.predict(input_comment)
+
+        end = int(round(time.time() * 1000))
+
+        print("Predicting: ", end - start)
 
         if clf_prediction[0][0] == CrossmodClassifiers.REMOVED_COMMENT:
             clf_prediction = 1
         else:
             clf_prediction = 0
 
+        print('PID is done now: ' + str(os.getpid()))
         # {'clfs_id': 'science', 'clfs_prediction': 1, 'clfs_type': SUBREDDIT }
+        
+        end_total = int(round(time.time() * 1000))
+
+        print("loading and predicting: ", end_total - start_total)
+
         return { 'clfs_id': clfs_id, 'clfs_prediction': clf_prediction, 'clfs_type': clfs_type }
         
     def get_result(self):
@@ -113,16 +137,6 @@ def preprocessing(input_comments):
     return train_text['text']
 
 def get_classifier_predictions(input_comments, subreddit_list):
-    #preprocess comments
-    comments = preprocessing(input_comments)
-    #write comment to a file
-    comment_to_file = open("temp_comments.txt", "w")
-    for comment in comments:
-        comment_to_file.write(comment)
-        comment_to_file.write("\n")
-
-    comment_to_file.close()
-
     count = 1
 
     ###result will be stored as a dataframe
@@ -142,12 +156,66 @@ def get_classifier_predictions(input_comments, subreddit_list):
         print(expert_decision)
     return test_comments
 
-def main():
-    print(CrossmodClassifiers(input_comment = "you suck", 
-                              clfs_type = CrossmodClassifiers.SUBREDDIT_CLASSIFIERS, 
-                              clfs_ids = ["technology", "Futurology", "space"]).get_result())
+def get_classifier_prediction(study_sub):
+    #input_comment = arguments['input_comment']
+    #study_sub = arguments['subreddit']
+    count = 1
 
-    # print(get_classifier_predictions(["space is really awesome!"], ["technology", "Futurology", "space"]))
+    ###result will be stored as a dataframe
+    test_comments = pd.DataFrame()
+
+    # print(count, ") Expert: " , study_sub)
+    count+=1
+    command = [CrossmodConsts.FASTTEXT_BINARY, "predict", CrossmodConsts.get_subreddit_classifier(study_sub), "temp_comments.txt", "1"]
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    expert_decision = output.split('\n')[:-1]
+    ###store each experts prediction as a new column, containing the subreddit's name
+    test_comments['prediction_' + study_sub] = expert_decision
+    test_comments['prediction_' + study_sub] = (test_comments['prediction_' + study_sub] == '__label__removed').astype(int)
+    print(expert_decision)
+    
+    return test_comments
+
+
+def main():
+    classifiers = []
+
+    import fileinput
+
+    for line in fileinput.input():
+        classifiers.append(line.replace('\n',''))
+
+    #print(CrossmodClassifiers(input_comment = "you suck", 
+    #                          clfs_type = CrossmodClassifiers.SUBREDDIT_CLASSIFIERS, 
+    #                          clfs_ids = classifiers))
+  
+    
+
+    arguments = [study_sub for study_sub in classifiers]
+
+    print(arguments)
+
+    #preprocess comments
+    comments = preprocessing(["you suck"])
+    
+    #write comment to a file
+    comment_to_file = open("temp_comments.txt", "w")
+    
+    for comment in comments:
+        comment_to_file.write(comment)
+        comment_to_file.write("\n")
+
+    comment_to_file.close()
+
+    print(get_classifier_prediction('AskReddit'))
+
+    pool = Pool()
+
+    results = pool.map(get_classifier_prediction, arguments)
+    pool.close()
+    pool.join()
+    #print(get_classifier_predictions(["space is really awesome!"], ["technology", "Futurology", "space"]))
 
 if __name__ == '__main__':
     main()
