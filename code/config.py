ed8a97ea997755906add154e9f2ddf03de64905d
Aditya Shylesh <adishy@umich.edu>
2020-01-22 12:49:32 -0500

    Updating moderator list

--- code/config.py
+++ code/config.py
@@ -16,16 +16,8 @@ def check_config(comment):
 
 	###conditional statements - to be modified by mods
 	try:
-		if comment['toxicity_score'] >= 0.9: 
-			ACTION = remove
-		elif comment['agreement_score'] >= 85: 
+		if comment['agreement_score'] >= 0.85: 
 			ACTION = report
-		elif comment['agreement_score'] >= 90 and comment['prediction_The_Donald'] == False:
-			ACTION = remove
-		elif comment['prediction_hatespeech-racist-homophobic'] == True and comment['norm_violation_score'] >= 6: 
-			ACTION = report
-		elif comment['prediction_misogynistic-slurs'] == True and comment['prediction_personal-attacks'] == True: 
-			ACTION = modmail
 	
 	### above this are the rules that are configured by the moderators
 
