ca9b9b7f573f603ac84884a713bf947e1e02067c
Aditya Manjusha Shylesh <adishy@umich.edu>
2020-02-27 04:12:45 +0000

    Adding tables for subreddit settings

---
+++ crossmod/db/tables/active_subreddits.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from crossmod.db.tables import SubredditSettingsTable 
+from crossmod.db.base import Base
+
+class ActiveSubredditsTable(Base):
+      __tablename__ = 'active_subreddits'
+      id = Column(Integer, primary_key = True)
+      subreddit = Column(String, ForeignKey('subreddit_settings.subreddit'))
+      perform_action = Column(Boolean)
