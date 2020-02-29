822c303d7bb580024955951a1d990b7cc7aefefb
Aditya Manjusha Shylesh <adishy@umich.edu>
2020-02-29 00:25:17 +0000

    Removing clf_ensemble.py

--- crossmod/ml/clf_ensemble.py
+++
@@ -1,5 +0,0 @@
-from crossmod.ml.classifiers import CrossmodClassifiers
-from crossmod.helpers.consts import CrossmodConsts
-
-clf_ensemble = CrossmodClassifiers(subreddits = CrossmodConsts.SUBREDDIT_LIST,
-                                   norms = CrossmodConsts.NORM_LIST)
