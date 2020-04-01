4a317e5fa89f9253b6fbbf42eadaac9e28c60e90
weijili <weijili@umich.edu>
2020-04-01 01:36:58 -0400

    working on retraining

--- crossmod/ml/retraining.py
+++ crossmod/ml/retraining.py
@@ -2,6 +2,9 @@ from crossmod.db.interface import CrossmodDB
 from crossmod.db.tables.data import DataTable
 from crossmod.helpers.consts import *
 import fasttext
+import datetime
+
+
 
 class retraining():
     def __init__(self):
@@ -10,7 +13,7 @@ class retraining():
 
         self.session = self.db.database_session
 
-    def train(self, subreddit):
+    def train(self, subreddit, start_time, end_time):
         '''
         sqlite> select count(*) from crossmoddbdata where banned_by is not null;
         7942
@@ -20,16 +23,20 @@ class retraining():
         4335
         '''
         rows = self.session.query(DataTable).filter(DataTable.subreddit == subreddit, 
-                                                    DataTable.crossmod_action == "report").limit(500)
+                                                    DataTable.crossmod_action == "report",
+                                                    DataTable.ingested_utc > start_time, 
+                                                    DataTable.ingested_utc <= end_time).limit(500)
 
-        model = fasttext.load_model(CrossmodConsts.get_subreddit_classifier(subreddit))
-
-        for row in rows:
-            print(model.predict(row.comment))
-
+        f = open("tmp_data.txt", "a")
         
+        for row in rows:
+            print(row)
+
+        f.close()
 
 
 if __name__ == "__main__":
     test = retraining()
-    test.train("Futurology")
+    start = 0
+    end = datetime.datetime.now()
+    test.train("Futurology", start, end)
