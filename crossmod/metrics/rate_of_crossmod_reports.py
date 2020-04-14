27b830354bc1e423d08e32cab4cf0e5c2d68f2d4
Aditya Manjusha Shylesh <adishy@pop-os.localdomain>
2020-04-13 21:44:44 -0700

    Added rate of moderator removals

--- crossmod/metrics/rate_of_crossmod_reports.py
+++ crossmod/metrics/rate_of_crossmod_reports.py
@@ -58,4 +58,4 @@ class RateOfCrossmodReports:
         line_plot = self.create_plot()
         plt.savefig(os.path.join(self.output_directory, f"{datetime.datetime.now()}.{self.output_format}"),  dpi=600)
 
-RateOfCrossmodReports("Futurology").show_plot()
+RateOfCrossmodReports("Futurology").save_plot()
